f3a33a00f367ba91ba6737a912ad4f69a16d5a17
David Mann <dmann@loggerhead.com>
2022-01-02 11:18:32 -0500

    Update serialRecordTest.ino

--- arduino/serialRecordTest/serialRecordTest.ino
+++ arduino/serialRecordTest/serialRecordTest.ino
@@ -1,9 +1,12 @@
 #define HWSERIAL Serial1
+#define REC_ST 8
+#define LED_GRN 17
 
 void setup() {
   Serial.begin(115200);
   HWSERIAL.begin(115200);
-
+  pinMode(REC_ST, INPUT);
+  pinMode(LED_GRN, OUTPUT);
 }
 
 void loop() {
@@ -13,7 +16,8 @@ void loop() {
     incomingByte = HWSERIAL.read();
     if (incomingByte==20) Serial.println();
     else Serial.write(incomingByte);
-    
   }
 
+  digitalWrite(LED_GRN, digitalRead(REC_ST));
+  
 }
